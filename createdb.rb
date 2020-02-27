eac918dd06f4852e9ac836e4d53142dfadeac3ad
huyenngk <huyen.nguyen@kellogg.northwestern.edu>
2020-02-27 17:01:32 +0000

    final_1

--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,51 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :restaurants do
   primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
-end
-DB.create_table! :rsvps do
-  primary_key :id
-  foreign_key :event_id
-  Boolean :going
   String :name
-  String :email
-  String :comments, text: true
+  String :description, text: true
+  String :popular_dishes, text: true
+  String :location
+  String :phone
+end
+DB.create_table! :comments do
+  primary_key :id
+  foreign_key :restaurant_id
+  String :name
+  String :reviews, text: true
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+restaurants_table = DB.from(:restaurants)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+restaurants_table.insert(name: "Nha Hang Viet Nam", 
+                    description: "We have specialties: Bun mam #83, Bun Cha Ca #89, Ca Thit Kho To #158, Canh chua ca #149, and other hot pots .....and more BYOB",
+                    popular_dishes: "Spring Rolls, Bun Bo Hue, Banh Xeo",
+                    location: "1032 W Argyle St Chicago, IL 60640",
+                    phone:"(773) 878-8895")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+restaurants_table.insert(name: "Hong Ngu", 
+                    description: "Simplicity, authenticity and a taste you won't easily forget.",
+                    popular_dishes: "Vietnamese Stone Bowl Pho, Spring Roll, Papaya Salad",
+                    location: "1113 W Argyle St Chicago, IL 60640",
+                    phone:"(773) 595-8888")
+
+restaurants_table.insert(name: "Lotus Cafe", 
+                    description: "Well-known Vietnamese sandwich specialist with a cozy ambiance, outdoor seating & fruit smoothies.",
+                    popular_dishes: "Honey Grilled Pork, Beef and Mushroom Banh Mi, Ginger Chicken",
+                    location: "719 W Maxwell St Chicago, IL 60607",
+                    phone:"(312) 733-7595")
+
+restaurants_table.insert(name: "Uptown Pho", 
+                    description: "Dine in, carry out, catering and delivery",
+                    popular_dishes: "Bun Bo Hue, Crab Ragoon, Pho Tai",
+                    location: "1010 W Argyle St Chicago, IL 60640",
+                    phone:"(773) 878-8820")
+
+restaurants_table.insert(name: "DaNang Kitchen", 
+                    description: "Come and check out our new restaurant in Chicago uptown! Call in or stop by for some
+vietnamese food! We are also BYOB!",
+                    popular_dishes: "Mi Quang, Banh Khot, Com Tam Suon Cha",
+                    location: "1019 W Argyle St Chicago, IL 60640",
+                    phone:"(773) 654-3564")
